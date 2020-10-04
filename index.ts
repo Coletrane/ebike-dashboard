583d53b68ed8999ff7562c2c4cfeb67e1c702db9
Cole <eloc49@gmail.com>
2020-10-04 09:49:29 -0400

    initial commit setting stuff up

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+import router from './routing'
+
+
+const app = new Application();
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
---
+++ service/db.ts
@@ -0,0 +1,1 @@
+import { CasualDB } from "https://deno.land/x/casualdb@0.1.1/mod.ts";
