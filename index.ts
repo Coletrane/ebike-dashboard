9689dac85a493c99c0194b788d7c026d17b5355d
Cole <eloc49@gmail.com>
2020-10-04 10:12:32 -0400

    got basic run configuration setup

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
-import router from './routing'
+import { APP_HOST, APP_PORT } from './config.ts'
+import router from './routing.ts'
 
-
-const app = new Application();
+const app = new Application()
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
+console.log(`Listening on ${APP_PORT}...`)
 
+await app.listen(`${APP_HOST}:${APP_PORT}`)
--- service/db.ts
+++ service/db.ts
@@ -1,1 +1,8 @@
-import { CasualDB } from "https://deno.land/x/casualdb@0.1.1/mod.ts";
+import { CasualDB } from 'https://deno.land/x/casualdb@0.1.1/mod.ts'
+
+// TODO: get some data and see what this looks like
+interface Schema {
+
+}
+
+const db = new CasualDB<Schema>()
